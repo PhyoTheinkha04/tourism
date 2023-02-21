7eb821154f99bd5009f7c29bbb4fbd9a876d6a0d
PhyoTheinkha04 <kokha042@gmail.com>
2023-02-21 17:34:48 +0630

    feb 21

--- login/login.php
+++ login/login.php
@@ -1,86 +1,128 @@
-
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Login Form</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <link rel="shortcut icon" type="image/icon" href="../assets/logo/favicon.png" />
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-</head>
-<body style="background-image: url('img2.jpg');">
-
-<?php
-        if(isset($_POST["submit"])){
-         $email = $_POST["email"];
-         $password = $_POST["password"];
-         echo $email . "-" . $password;
-
-         registerUser($username,$email,$password);
-        }
+<?php 
+// Start session 
+session_start(); 
+ 
+// Get data from session 
+$sessData = !empty($_SESSION['sessData'])?$_SESSION['sessData']:''; 
+ 
+// Get status from session 
+if(!empty($sessData['status']['msg'])){ 
+    $statusMsg = $sessData['status']['msg']; 
+    $status = $sessData['status']['type']; 
+    unset($_SESSION['sessData']['status']); 
+} 
+ 
+$postData = array(); 
+if(!empty($sessData['postData'])){ 
+    $postData = $sessData['postData']; 
+    unset($_SESSION['postData']); 
+} 
+ 
+// If the user already logged in 
+if(!empty($sessData['userLoggedIn']) && !empty($sessData['userID'])){ 
+    include_once 'User.class.php'; 
+    $user = new User(); 
+    $conditions['where'] = array( 
+        'id' => $sessData['userID'] 
+    ); 
+    $conditions['return_type'] = 'single'; 
+    $userData = $user->getRows($conditions); 
+} 
 ?>
-    <div class="container">
-       
+
+<?php if(!empty($userData)){ ?>
+<h2>Welcome <?php echo $userData['username']; ?>!</h2>
+
+<a href="userAccount.php?logoutSubmit=1" class="logout">Logout</a>
+
+<div class="regisFrm">
+
+    <p><b>Name: </b><?php echo $userData['username']; ?></p>
+    <p><b>Email: </b><?php echo $userData['email']; ?></p>
+</div>
+<?php include '/index.php';?>
+<?php }else{ ?>
+
+<!-- Status message -->
+<?php if(!empty($statusMsg)){ ?>
+<div class="status-msg <?php echo $status; ?>"><?php echo $statusMsg; ?></div>
+<?php } ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="shortcut icon" type="image/icon" href="../assets/logo/favicon.png" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>login</title>
+    <!-- Bootstrap CSS -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.2/css/bootstrap.min.css'>
+    <!-- Font Awesome CSS -->
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.3.1/css/all.css'>
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/reg.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+</head>
+
+<body>
+
+
+    <div class="container2" style="width: 80%;">
         <div class="row px-3">
             <div class="col-lg-10 col-xl-9 card flex-row mx-auto px-0">
                 <div class="img-left d-none d-md-flex"></div>
-               
                 <div class="card-body">
-                    <h4 class="title text-center mt-4">
-                        Login into account
-                    </h4>
-                    <form action="login.php" class="form-box px-3" method="post">
-                        <div class="form-input">
-                            <span><i class=""></i></span>
-                            <input type="email" name="email" id="email" placeholder="Email Address" tabindex="10" required>
-                        </div>
-                        <div class="form-input">
-                            <span><i class=""></i></span>
-                            <input type="password"  id="password"  name="password" placeholder="Password" required>
-                        </div>
+                    <div class="d-flex flex-column text-center">
+                        <span><i class=""> </i></span>
+                        <h2 style="margin-bottom:50px;">Login to Your Account</h2>
+                        <div class="regisFrm">
+                            <form action="userAccount.php" class="form-box px-3" method="post">
+                                <div class="form-input">
+                                    <div class="input-group">
+                                        <input autocomplete="off" name="email" id="email" class="input" type="email"
+                                            placeholder="EMAIL"
+                                            value="<?php echo !empty($postData['email'])?$postData['email']:''; ?>"
+                                            required="">
+                                    </div>
+                                </div>
+                                <div class="form-input">
+                                    <div class="input-group">
+                                        <input utocomplete="off" type="password" name="password" id="password"
+                                            class="input" placeholder="PASSWORD" required="">
 
-                        <div class="mb-3">
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="cb1" name="">
-                                <label class="custom-control-label" for="cb1">Remember me</label>
-                            </div>
-                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-3 send-button">
+                                    <div class="send-button">
+                                        <button type="submit" name="loginSubmit">submit</button>
+                                    </div>
+                                </div>
 
-                        <div class="mb-3">
-                            <button type="submit" id="submit" name="submit" class="btn btn-block text-uppercase">
-                                Login
-                            </button>
-                        </div>
 
-                        <div class="text-right">
-                            <a href="forgetpass.php" class="forget-link">
-                                Forget Password?
-                            </a>
+                                <p>Don't have an account? <a href="register.php">Register</a></p>
                         </div>
-                        <br>
-                        <div class="text-right" style="margin: top 30px;">
-                            <a href="../views/home_page.php" class="forget-link" >
-                                Go Back To Main Page
-                            </a>
-                        </div>
-                        
-
-                        <hr class="my-4">
-
-                        <div class="text-center mb-2">
-                            Don't have an account?
-                            <a href="../php_login_system/registration.php" class="register-link">
-                                Register here
-                            </a>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</body>
 
-</html>
+
+    </div>
+
+    </div>
+    <footer>
+        <!-- jQuery -->
+        <script src='../assets/js/jquery.min.js'></script>
+
+
+
+        <script src='../assets/js/bootstrap.min.js'></script>
+        <!-- Custom Script -->
+
+    </footer>
+</body>
+<?php } ?>
